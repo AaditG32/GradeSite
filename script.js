31751d84fcc1bdd4f75a43d267f4dd56f4e06590
AaditG32 <109329006+AaditG32@users.noreply.github.com>
2022-09-22 14:02:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+username = user-name;
+password = pass-word;
+
 
 const StudentVue = require('studentvue.js');
-StudentVue.login('district url', 'username', 'password')
+StudentVue.login('https://sisstudent.fcps.edu', username, password)
     .then(client => client.getMessages())
     .then(console.log);
 
