468df2598c085906ff549b93565e0fae1b9375f8
AaditG32 <109329006+AaditG32@users.noreply.github.com>
2022-09-23 10:14:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ console.log("hello world");
 var btn = document.getElementById('sub-mit');
 btn.addEventListener('click', func);
 
+var usern = document.getElementById('user-name');
+var passw = document.getElementById('pass-word')
 
 
 const StudentVue = require('studentvue.js');
 
 function func() {
-    StudentVue.login('https://sisstudent.fcps.edu', user-name, pass-word)
+    StudentVue.login('https://sisstudent.fcps.edu', usern, passw)
         .then(client => client.getMessages())
         .then(studentVueClient.getGradebook())
         .then(console.log);
